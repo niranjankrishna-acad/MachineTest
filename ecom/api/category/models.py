26c508db3c1f8be87e7f9bbd6bf1376f3d7c1954
Maneesh <maneeshsriram123@gmail.com>
2021-01-01 13:27:52 +0530

    initial commit

---
+++ ecom/api/category/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
