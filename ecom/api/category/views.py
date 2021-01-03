a659479fdd20418f56f34b2349627b9c95529aba
Maneesh <maneeshsriram123@gmail.com>
2021-01-03 14:24:54 +0530

    progress commit

--- ecom/api/category/views.py
+++ ecom/api/category/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .serialize import CategorySerializer
+from .models import Category
 
 # Create your views here.
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
