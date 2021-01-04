af9c5d07b45c7de9527ab9dba779ef88afc6978d
Maneesh <maneeshsriram123@gmail.com>
2021-01-04 10:24:18 +0530

    Update models.py

--- ecom/api/user/models.py
+++ ecom/api/user/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# Creating model based on existing model
+
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=30, default='Anonymous')
+    email = models.CharField(max_length=100, unique=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    phone = models.CharField(max_length=12, blank=True, null=True)
+    gender = models.CharField(max_length=8, blank=True, null=True)
+
+    session_token = models.CharField(max_length=10, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
