416072ac414d82adf522ca78423704aa6a6c4481
Maneesh <maneeshsriram123@gmail.com>
2021-01-06 15:22:07 +0530

    Update ImageHelper.js

--- frontend/src/core/helper/ImageHelper.js
+++ frontend/src/core/helper/ImageHelper.js
@@ -5,7 +5,7 @@ const ImageHelper = ({ product }) => {
 
     return (
         <div className="rounded border  border-success p-2">
-            <img src={imageURL} style={{ maxHeight: "100%", maxWidth: "100%" }} className="mb-3 rounded" alt="image" />
+            <img src={imageURL} style={{ maxHeight: "100%", maxWidth: "100%" }} className="rounded" alt="image" />
         </div>
     )
 }
