0a6ebf788f11931686bb951cbc8faee30956b6b8
Maneesh <maneeshsriram123@gmail.com>
2021-01-06 09:32:35 +0530

    update imagehelper

---
+++ frontend/src/core/helper/ImageHelper.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ImageHelper = ({ product }) => {
+    const imageURL = product ? product.image : `https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/04/10/16/online-clothes-shops-hero.jpg`
+
+    return (
+        <div className="rounded border  border-success p-2">
+            <img src={imageURL} style={{ maxHeight: "100%", maxWidth: "100%" }} className="mb-3 rounded" alt="image" />
+        </div>
+    )
+}
+
+export default ImageHelper
--- frontend/src/core/helper/coreapicalls.js
+++ frontend/src/core/helper/coreapicalls.js
@@ -1,12 +1,11 @@
 import { API } from '../../backend'
 
-const coreapicalls = () => {
-    return fetch(`${API}product`, { method: "GET" })
+export const getProducts = () => {
+    return fetch(`${API}product/`, { method: "GET" })
         .then(response => {
             return response.json()
         })
-
         .catch(err => console.log(err))
 }
 
-export default coreapicalls
+
