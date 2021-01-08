630e0fe202748366eae2416d9a26418b41a71e07
Maneesh <maneeshsriram123@gmail.com>
2021-01-08 10:30:02 +0530

    update

--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import Base from './Base'
 import { getProducts } from './helper/coreapicalls'
+import '../styles.css'
+import Card from './Card'
 
 const Home = () => {
 
@@ -24,17 +27,17 @@ const Home = () => {
     }, [])
 
     return (
-        <div>
+        <Base title="Home Page" descr="Welcome to the store">
             <div className="row">
                 {products.map((product, index) => {
                     return (
-                        <div key={index}>
-                            <h1>{product.name}</h1>
+                        <div key={index} className="col-4 mb-4">
+                            <Card product={product} />
                         </div>
                     )
                 })}
             </div>
-        </div>
+        </Base>
     )
 }
 
---
+++ frontend/src/core/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+const currentTab = (history, path) => {
+    if (history.location.pathname === path) {
+        return { color: "#2ecc72" }
+    } else {
+        return { color: "#FFFFFF" }
+    }
+}
+
+
+const Menu = ({ history, path }) => {
+    return (
+        <div>
+            <ul className="nav nav-tabs bg-dark">
+                <li className="nav-item">
+                    <Link style={currentTab(history, "/")} className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link style={currentTab(history, "/signin")} className="nav-link" to="/">Sign In</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default withRouter(Menu)
