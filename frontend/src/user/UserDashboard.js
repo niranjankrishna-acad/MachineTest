2ff94e7478d0ee9bf7a3d94858a4647184faf1c1
Maneesh <maneeshsriram123@gmail.com>
2021-01-08 11:59:45 +0530

    Update UserDashboard.js

---
+++ frontend/src/user/UserDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Base from '../core/Base'
+
+const UserDashboard = () => {
+    return (
+        <div>
+            <Base title="User Dashboard" descr="Welcome to your area">
+
+            </Base>
+        </div>
+    )
+}
+
+export default UserDashboard
