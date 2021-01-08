81a60a15ce54822c408661c47b2f41ea50cdb4db
Maneesh <maneeshsriram123@gmail.com>
2021-01-08 10:28:03 +0530

    Update Routes.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import PrivateRoutes from './auth/helper/PrivateRoutes'
 import Home from './core/Home'
+import Signup from './user/Signup'
 
 const Routes = () => {
     return (
         <Router>
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/signup" exact component={Signup} />
                 {/*<PrivateRoutes path="/user/dashboard" exact component={ } />*/}
             </Switch>
         </Router>
