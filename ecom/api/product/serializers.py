2ec21ec236be998c31e1584a27727534240be412
Maneesh <maneeshsriram123@gmail.com>
2021-03-24 00:47:06 +0530

    Update serializers.py

--- ecom/api/product/serializers.py
+++ ecom/api/product/serializers.py
@@ -5,7 +5,8 @@ from .models import Product
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     image = serializers.ImageField(
-        max_length=None, allow_empty_file=False, allow_null=True, required=False)
+        max_length=None, allow_empty_file=False, allow_null=True, required=False
+    )
 
     class Meta:
         model = Product
