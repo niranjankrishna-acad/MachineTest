25dc36984164d8677b0a20cf0a67655f45f14c74
Maneesh <maneeshsriram123@gmail.com>
2021-03-26 19:39:05 +0530

    Update admin.py

--- ecom/api/order/admin.py
+++ ecom/api/order/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 from .models import Order
 # Register your models here.
 
-admin.site.register(Order)
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('user', 'product_names', 'total_products',
+                    'transaction_id', 'total_amount')
+
+
+admin.site.register(Order, OrdersAdmin)
